3a5d41a26953626d53c94c1f2e96b26d2169d126
Dan Bar-Yaakov <dan.bar-yaakov@luminis.eu>
2017-09-14 19:09:48 +0300

    change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hello: 'there' });
+    res.send({ hello: 'buddy' });
 });
 
 const PORT = process.env.PORT  || 5000;
